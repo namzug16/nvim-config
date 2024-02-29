87a461e31fc657233b4e664f9813c7587def75a1
Gustavo Guzman <guzmanlo.2001@gmail.com>
2024-02-29 16:57:27 +0100

    FIX: telescope colors

--- init.lua
+++ init.lua
@@ -1,15 +1,31 @@
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.api.nvim_set_option("clipboard", "unnamed")
 
-vim.g.mapleader = " "
-
 -- numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "number"
 
 -- windows
 vim.keymap.set("n", "<leader>wsh", ":split<CR>", { silent = true })
@@ -23,21 +39,15 @@ vim.keymap.set("n", "<leader>wc", ":q<CR>", { silent = true })
 -- save all files
 vim.keymap.set("n", "<leader>wa", ":wa<CR>", {})
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
-
-vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#00FF00", bg = "NONE" })
+-- colors
+local color = "#00FF00"
+vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = color, bg = "NONE" })
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = color, bg = "NONE" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = color, bg = "NONE" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = color, bg = "NONE" })
+vim.api.nvim_set_hl(0, "TelescopePromptCounter", { fg = color, bg = "NONE" })
+vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = color, bg = "NONE" })
+vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = color, bg = "NONE" })
+vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = color, bg = "NONE" })
+vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = color, bg = "NONE" })
+vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { fg = color, bg = "NONE" })
