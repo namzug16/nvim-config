72b5bf7e227d94fd94eb4b438eeaa22c22a092a5
Gustavo Guzman <guzmanlo.2001@gmail.com>
2024-02-29 13:26:26 +0100

    FEAT: dap initial configuration

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,1 +1,33 @@
-return {}
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+		},
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
+
+      dap.setup({})
+      dapui.setup({})
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+			vim.keymap.set("n", "<leader>dc", dap.continue, {})
+		end,
+	},
+}
